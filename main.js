a107ca3e132900ad70fdc23823caffa000e40d8b
Lincoln Atkinson <LINCOLN.ATKINSON2478@STU.MTEC.EDU>
2023-08-31 23:06:29 -0600

    attempted animation

---
+++ main.js
@@ -0,0 +1,20 @@
+const button = document.getElementById('pageButton');
+const homepage = document.querySelector('.home');
+const homeE = document.querySelector('.homeE');
+const settingsPage = document.querySelector('.settings');
+
+/*
+button.addEventListener('click', () => {
+    var pos = 0;
+    setInterval (function () {
+        //homepage.style.bottom = pos + 'px';
+        //homeE.style.bottom = pos + 'px';
+        
+        //homepage.style.opacity = pos;
+        pos++;
+        if (pos = 350) {
+            clearInterval();
+        }
+    }, 500);    
+});
+*/
